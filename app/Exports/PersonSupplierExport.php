570ca6deb4d5dba474f760192ec5d1c40745cdb4
subhanulrizqi <muhammadsubhanulrizqi@gmail.com>
2025-06-24 14:41:47 +0700

    update export excel

--- app/Exports/PersonSupplierExport.php
+++ app/Exports/PersonSupplierExport.php
@@ -4,28 +4,41 @@ namespace App\Exports;
 
 use App\Models\MasterPersonSupplier;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class PersonSupplierExport implements FromCollection
+class PersonSupplierExport implements FromCollection,WithHeadings
 {
     public function collection()
     {
         return MasterPersonSupplier::select(
-            'description',
-            'contact_person_01',
-            'phone_02',
-            'contact_person_02',
-            'email_02',
-            'contact_person_03',
-            'email_03'
+            'manual_id',               // Supplier Code
+            'description',             // Supplier Name
+            'prefix',                  // Initials
+            'address_01',              // Address
+            'phone_01',                // Phone
+            'fax_01',                  // Fax
+            'contact_person_01',       // Con. Person Name
+            'phone_02',                // Con. Person Phone
+            'email_01',                // Email
+            'contact_person_02',       // WH/Del PIC Name
+            'email_02',                // WH/Del PIC Email
+            'contact_person_03',       // QC PIC Name
+            'email_03'                 // QC PIC Email
         )->get();
     }
 
     public function headings(): array
     {
         return [
-            'Initial',
+            'Supplier Code',
+            'Supplier Name',
+            'Initials',
+            'Address',
+            'Phone',
+            'Fax',
             'Con. Person Name',
             'Con. Person Phone',
+            'Email',
             'WH/Del PIC Name',
             'WH/Del PIC Email',
             'QC PIC Name',
