1262264465c92013df6a0337017efe1010fab0c4
subhanulrizqi <muhammadsubhanulrizqi@gmail.com>
2025-06-29 00:25:03 +0700

    update sku export

--- app/Exports/SkuExport.php
+++ app/Exports/SkuExport.php
@@ -1,7 +1,7 @@
 <?php
 namespace App\Exports;
 
-use App\Models\MasterSku;
+use App\Models\VwMasterSkuPartInformation;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
 use Maatwebsite\Excel\Concerns\WithHeadings;
@@ -10,24 +10,25 @@ class SkuExport implements FromCollection, WithHeadings, ShouldAutoSize
 {
    public function collection()
     {
-        return MasterSku::select(
-            'manual_id',    //Part Code
-            'description',  //Part Name
-            'specification_code', //Specification code
-            'specification_detail', //Specification Description
-            'sku_business_type_id',   //Sales category
-            'group_tag',    //Set Code
-            'sku_type_id',    //Item Sub Category
-            'sku_type_id',    //Item type
-            'sku_model_id', //model
-            'val_area', //surace area
-            'val_weight',   //weight
-            'sku_inventory_unit_id',    //inventory unit
-            'flag_sku_procurement_type',    //procurement type
-            'sku_procurement_unit_id',   //procurement unit
-            'val_conversion',   //conversion'
-            'flag_inventory_register',  //inventory register
-        )->where('flag_sku_type', 1)->get();
+        return VwMasterSkuPartInformation::select(
+            'part_code',    //Part Code
+            'part_name',  //Part Name
+            'Specification_Code', //Specification code
+            'Specification_Description', //Specification Description
+            'Sales_category',   //Sales category
+            'set_code',    //Set Code
+            'Item_Sub_Category',    //Item Sub Category
+            'Item_type',    //Item type
+            'business_type', //model
+            'model', //surace area
+            'surace_area',   //weight
+            'weight',    //inventory unit
+            'inventory_unit',    //procurement type
+            'procurement_type',   //procurement unit
+            'procurement_unit',   //conversion'
+            'conversion',  //inventory register
+            'inventory_register',  //inventory register
+        )->get();
     }
 
     public function headings(): array
